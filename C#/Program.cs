84d8250ed24ca2e61707d4c4e69870dc477fceb8
Dipali8451 <dipaspatil84@gmail.com>
2025-04-21 16:54:18 +0530

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Programming World!");
+int a = 10;
+int b = 20;
+int c = a + b;
+Console.WriteLine("The Addition Of Two Number Is:" + c+"");
+int d = a - b;
+Console.WriteLine("The Substraction Of Two Number Is:"+d+"");
